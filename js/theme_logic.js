d7146f1e1d96a8d0f65df7d74ed23eb0eb3e4fb7
Samuel <samuel.rodriguez@outlook.com>
2020-04-13 20:15:53 -0500

    the api is being in progress

--- js/theme_logic.js
+++ js/theme_logic.js
@@ -1,4 +1,16 @@
-menuThemeTrigger.addEventListener("click",showingMenu)
-sailorDay.addEventListener("click", dayTheme)
-sailorNight.addEventListener("click", darkTheme)
-
+menuThemeTrigger.addEventListener("click", showingMenu);
+sailorDay.addEventListener("click", dayTheme);
+sailorNight.addEventListener("click", darkTheme);
+url = "https://api.giphy.com/v1/gifs/random?api_key=qsOjAmeDhQKoL3IW1Cnaty7Rayav17Ix&tag=&rating=G";
+async function gettingInformation(url) {
+  const testing = await fetch(url);
+  return testing.json();
+}
+gettingInformation(url)
+  .then((result) => {
+    console.log(result.data);
+    document.getElementById("nuevaprueba").setAttribute("src",result.data.images.preview_webp.url)
+  })
+  .catch((error) => {
+    console.log("El error fue " + error);
+  });
